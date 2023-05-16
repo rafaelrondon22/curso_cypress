903fe9a73024708a4e1155abd39c0cb9dc178e3c
Rafa <rafaelrondon22@gmail.com>
2023-05-16 02:23:54 -0300

    Agregar Mocha.cy.js

--- cypress/e2e/mocha.cy.js
+++ cypress/e2e/mocha.cy.js
@@ -1,1 +1,20 @@
-describe('Priema suite de pruebas',)
+describe('Primera suite de pruebas', () => {
+    const profesor = 'Fabrizio'
+
+    it('Primer test con mocha', () => {
+        cy.log('Primer test con cypress con ' + profesor)
+    })
+
+    it('Segundo test con mocha', () => {
+        cy.log('Segundo test con cypress  con ' + profesor)
+    })
+
+    it('Tercer test con mocha', () => {
+        cy.log('Tercer test con cypress  con ' + profesor)
+    })
+})
+
+it.skip('Tercer test con mocha', () => {
+    cy.log('Tercer test con cypress  con ' + profesor)
+})
+
